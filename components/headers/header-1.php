ff4df3c1c64d8a62999bd812c2957a367ee4e23c
Eman Al Masri <emanma@aworldit.com>
2024-06-11 10:33:54 +0300

    responsive touches

--- components/headers/header-1.php
+++ components/headers/header-1.php
@@ -7,8 +7,8 @@ $pageName = pathinfo($pageName, PATHINFO_FILENAME);
 
 ?>
 
-<header class="header <?php if($pageName=="index"){?>white-header<?php } ?>">
-    <div class="container mod">
+<header class="header headerbig <?php if($pageName=="index"){?>white-header<?php } ?>">
+<div class="container mod">
         <nav class="navbar navbar-expand-lg navbar-light">
             <a class="navbar-brand" href="index">
                 <?php if($pageName=="index"){?><img src="assets/images/white-logo.svg" alt="Logo"
@@ -56,6 +56,107 @@ $pageName = pathinfo($pageName, PATHINFO_FILENAME);
                         <a class="nav-link" href="https://dashboard.matensa.com/#/auth/login">Login</a>
                     </li>
                     <li class="nav-item nav-item-btn">
+                        <a class="nav-link btn-style-1" href="https://dashboard.matensa.com/#/auth/register">
+                            <i><svg width="20" height="20" viewBox="0 0 20 20" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M14.1414 16.2539C13.9381 14.6077 12.5345 13.3333 10.8333 13.3333H9.16667C7.46548 13.3333 6.06194 14.6077 5.85864 16.2539M14.1414 16.2539C16.1655 14.9108 17.5 12.6112 17.5 10C17.5 5.85786 14.1421 2.5 10 2.5C5.85786 2.5 2.5 5.85786 2.5 10C2.5 12.6112 3.83447 14.9108 5.85864 16.2539M14.1414 16.2539C12.9547 17.0413 11.5309 17.5 10 17.5C8.46911 17.5 7.04534 17.0413 5.85864 16.2539M12.5 8.33333C12.5 9.71405 11.3807 10.8333 10 10.8333C8.61929 10.8333 7.5 9.71405 7.5 8.33333C7.5 6.95262 8.61929 5.83333 10 5.83333C11.3807 5.83333 12.5 6.95262 12.5 8.33333Z"
+                                        stroke="white" stroke-width="1.5" stroke-linecap="round"
+                                        stroke-linejoin="round" />
+                                </svg>
+                            </i>
+                            Sign Up </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</header>
+
+<header class="header headersmall <?php if($pageName=="index"){?>white-header<?php } ?>">
+    <div class="container mod">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="index">
+                <?php if($pageName=="index"){?><img src="assets/images/white-logo.svg" alt="Logo"
+                    class="header-height"><?php }else{?>
+                <img src="assets/images/logo.svg" alt="Logo" class="header-height">
+                <?php } ?>
+
+            </a>
+            <a class="navbar-toggler" onclick="showMenu()" type="button" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-label="Toggle navigation">
+                <span class="menu-bar"><span></span><span></span><span></span></span>
+            </a>
+
+            <span class="menu-mobile-shadow"></span>
+            <div class="collapse navbar-collapse show ml-auto" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <!-- <li class="nav-item <?php if($pageName=="index"){?>active<?php } ?>">
+                        <a class="nav-link" href="index">Home</a>
+                    </li> -->
+                    <li
+                        class="nav-item dropdown nodown <?php if($pageName=="growbusiness" || $pageName=="agreementoverview" || $pageName=="transactionhistory" || $pageName=="businessintegration"){?>active<?php } ?>">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Matensa for Business
+                        </a>
+                        <div class="dropdown-menu" >
+                            <a class="dropdown-item" href="growbusiness">Grow your Business</a>
+                            <a class="dropdown-item" href="agreementoverview">Agreements Overview</a>
+                            <!-- <div class="dropdown-divider"></div> -->
+                            <a class="dropdown-item" href="transactionhistory">Transaction History</a>
+                            <a class="dropdown-item" href="businessintegration">Seamless Integration</a>
+
+                        </div>
+                    </li>
+                    <li
+                        class="nav-item dropdown nodown <?php if($pageName=="growbusiness" || $pageName=="agreementoverview" || $pageName=="transactionhistory" || $pageName=="businessintegration"){?>active<?php } ?>">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Personal
+                        </a>
+                        <div class="dropdown-menu" >
+                            <a class="dropdown-item" href="growbusiness">Refine Presence</a>
+                            <a class="dropdown-item" href="agreementoverview">Secure Agreements</a>
+                            <!-- <div class="dropdown-divider"></div> -->
+                            <a class="dropdown-item" href="transactionhistory">Cash out</a>
+                            <a class="dropdown-item" href="businessintegration">Top up</a>
+                            <a class="dropdown-item" href="businessintegration">Check Transactions</a>
+                        </div>
+                    </li>
+                  
+                    <li
+                        class="nav-item dropdown <?php if($pageName=="growbusiness" || $pageName=="agreementoverview" || $pageName=="transactionhistory" || $pageName=="businessintegration"){?>active<?php } ?>">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Company <img src="assets/images/content/Vector (2).svg">
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="aboutus">About</a>
+                            <a class="dropdown-item" href="jointeam">Careers</a>
+                            <!-- <div class="dropdown-divider"></div> -->
+                            <a class="dropdown-item" href="contactus">Contact</a>
+                            <a class="dropdown-item" href="faq">FAQ</a>
+                        </div>
+                    </li>
+                    <li
+                        class="nav-item dropdown <?php if($pageName=="growbusiness" || $pageName=="agreementoverview" || $pageName=="transactionhistory" || $pageName=="businessintegration"){?>active<?php } ?>">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Support <img src="assets/images/content/Vector (2).svg">
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" >Help Center</a>
+                            <a class="dropdown-item" href="reportbug">Report a Bug</a>
+                            <!-- <div class="dropdown-divider"></div> -->
+                        </div>
+                    </li>
+                    <li class="nav-item nav-item-btn">
+                    <a class="nav-link btn-style-3 mr-2" href="https://dashboard.matensa.com/#/auth/login">
+                            <i>
+                            </i>
+                            Log in
+                             </a>
                         <a class="nav-link btn-style-1" href="https://dashboard.matensa.com/#/auth/register">
                             <i><svg width="20" height="20" viewBox="0 0 20 20" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
